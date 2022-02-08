eb688fea1ace04dd65daadef34fbccddcaa7f024
Alexandru Berende <alexandruberende@gmail.com>
2022-02-08 16:59:02 +0100

    Initial Commit: Project Structure. HW Parts. Test SW.

---
+++ Pet_Feeder_Software/Pet_Feeder_Software.ino
@@ -0,0 +1,132 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// Define Constants
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+// Connections to A4988
+const int dirPin = 33;  // Direction
+const int stepPin = 32; // Step
+
+// Motor steps per rotation
+const int STEPS_PER_REV = 200;
+ 
+void setup() {
+    Serial.begin(9600);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+  
+  // Setup the pins as Outputs
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
+}
+
+void testdrawtriangle(void) {
+  display.clearDisplay();
+
+  for(int16_t i=0; i<max(display.width(),display.height())/2; i+=5) {
+    display.drawTriangle(
+      display.width()/2  , display.height()/2-i,
+      display.width()/2-i, display.height()/2+i,
+      display.width()/2+i, display.height()/2+i, SSD1306_WHITE);
+    display.display();
+    delay(1);
+  }  
+}
+
+void testfilltriangle(void) {
+  display.clearDisplay();
+
+  for(int16_t i=max(display.width(),display.height())/2; i>0; i-=5) {
+    // The INVERSE color is used so triangles alternate white/black
+    display.fillTriangle(
+      display.width()/2  , display.height()/2-i,
+      display.width()/2-i, display.height()/2+i,
+      display.width()/2+i, display.height()/2+i, SSD1306_INVERSE);
+    display.display();
+    delay(1);
+  } 
+}
+
+void testdrawroundrect(void) {
+  display.clearDisplay();
+
+  for(int16_t i=0; i<display.height()/2-2; i+=2) {
+    display.drawRoundRect(i, i, display.width()-2*i, display.height()-2*i,
+      display.height()/4, SSD1306_WHITE);
+    display.display();
+    delay(1);
+  }
+
+  
+}
+
+void testfillroundrect(void) {
+  display.clearDisplay();
+
+  for(int16_t i=0; i<display.height()/2-2; i+=2) {
+    // The INVERSE color is used so round-rects alternate white/black
+    display.fillRoundRect(i, i, display.width()-2*i, display.height()-2*i,
+      display.height()/4, SSD1306_INVERSE);
+    display.display();
+    delay(1);
+  }
+}
+void loop() {
+  
+  // Set motor direction clockwise
+  digitalWrite(dirPin,HIGH); 
+
+  testdrawroundrect();
+  testfillroundrect();
+  
+  // Spin motor one rotation slowly
+  for(int x = 0; x < STEPS_PER_REV; x++) {
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(2000); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(2000); 
+  }
+  
+  // Pause for one second
+  delay(1000); 
+
+  testdrawtriangle(); 
+  testfilltriangle();
+  
+  // Set motor direction counterclockwise
+  digitalWrite(dirPin,LOW);
+  
+  // Spin motor two rotations quickly
+  for(int x = 0; x < (STEPS_PER_REV * 2); x++) {
+    digitalWrite(stepPin,HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(stepPin,LOW);
+    delayMicroseconds(1000);
+  }
+  
+  // Pause for one second
+  delay(1000);
+  display.clearDisplay();
+
+  // Pause for two second
+  delay(2000);
+}
